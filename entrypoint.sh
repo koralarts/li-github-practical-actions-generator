35d4d33fb5006e7c60b0620e7ad12fb23169314c
Karl Castillo <khcastillo@hotmail.com>
2025-06-14 13:30:10 +0000

    Add podcast feed generator action

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#! bin/sh
+
+echo "=============="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git congig --global --add safe.directory /github/workspace
+
+python /usr/bin/feed.py
+
+git add .
+git commit -m "Update feed"
+git push --set-upstream origin main
+
+
+echo "=============="
